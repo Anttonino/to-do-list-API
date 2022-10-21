ce957e503122ef0417703386cc7c10fa9d2d3082
Anttonino <victor.antonino12@gmail.com>
2022-10-21 15:41:02 -0300

    docs: create all functions documentations in list service

--- src/list/services/list.service.ts
+++ src/list/services/list.service.ts
@@ -11,11 +11,22 @@ export class listService {
         @InjectRepository (List)
         private listRepository: Repository <List>
     ) { }
-
+    
+    /**
+     * @desc find all to-do in repository
+     * @example
+     * list | The program will show all to_do in database
+     */
     async findAll (): Promise <List []> {
         return await this.listRepository.find ()
     }
 
+    /**
+     * @desc find to-do by id
+     * @param id 
+     * @example
+     * find list/2 | The program will search the id to_do 2 in its database
+     */
     async findById (id: number): Promise <List> {
         let searchList = await this.listRepository.findOne ({
             where: {id}
@@ -25,10 +36,23 @@ export class listService {
         return searchList;
     }
 
+    /**
+     * @desc create a new to-do
+     * @param List 
+     * @example
+     * {"text": "wake-up", "done": true}
+     * {"text": "brush my teeths", "done": false}
+     */
     async create (list: List): Promise <List> {
         return await this.listRepository.save (list)
     }
 
+    /**
+     * @desc update a new to-do
+     * @param List 
+     * @example
+     * {"id": 1,"text": "brush my teeths", "done": true}
+     */
     async update (list: List): Promise <List> {
         let searchList: List = await this.findById (list.id)
 
@@ -37,6 +61,12 @@ export class listService {
         return await this.listRepository.save (list)
     }
 
+    /**
+     * @desc delete a to-do in database
+     * @param id 
+     * @example
+     * list/2 | The program will search the id to_do 2 and delete him
+     */
     async delete (id: number): Promise <DeleteResult> {
         let searchList = await this.findById (id);
 
